4af055876244f4c77b2a00edaaba5ead97c905d4
whtsky <whtsky@gmail.com>
2018-04-04 13:29:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# antd-typescript-boilerplate
+
+For creating reproduction about Ant Design's TypeScript-related issue.
+
+## Usage
+```
+npm install
+// edit your code
+npm start
+```
+Then open `http://localhost:8080/` in your browser
---
+++ index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import { DatePicker, version } from 'antd';* 
+import 'antd/dist/antd.css';
+
+ReactDOM.render(
+  <div style={{ margin: 24 }}>
+    <p style={{ marginBottom: 24 }}>
+      {/* Current antd version: {version} <br/> */}
+    </p>
+  </div>,
+  document.getElementById('root')
+);
